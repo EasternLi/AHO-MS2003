EasternLi/AHO-MS2003

commit bee40b69f5163dbef8e393ef020e9e837e279c89

use nullptr in lct

3rd/lct/LCT.cpp
@@ -3,39 +3,32 @@
 
 #include "LCT.hpp"
 #include <algorithm>
-#include <limits>
 #include <cassert>
 
-static LCT::Node *null = []() {
-	auto p = new LCT::Node;
-	p->init(std::numeric_limits<Data>::infinity());
-	return p;
-}();
-
 void LCT::Node::init(Data _val) {
-	fa = ch[0] = ch[1] = null;
+	fa = ch[0] = ch[1] = nullptr;
 	val = min = {_val, this};
 	add = 0;
 }
 
 LCT::Node *LCT::Node::find_root() {
 	Node *x = access();
-	while (x->ch[0] != null)
+	while (x->ch[0])
 		x = x->ch[0];
 	return x;
 }
 
 void LCT::Node::set_fa(Node *_fa) {
 	splay();
-	assert(fa == null);
+	assert(!fa);
 	fa = _fa;
 }
 
 void LCT::Node::cut() {
 	access();
 	assert(ch[0]);
-	ch[0]->fa = null;
-	ch[0] = null;
+	ch[0]->fa = nullptr;
+	ch[0] = nullptr;
 	push_up();
 }
 
@@ -62,22 +55,24 @@ void LCT::Node::add_val_way_to_root(Data d) {
 	update_add(d);
 }
 
-void LCT::Node::push_up() { min = std::min({val, ch[0]->min, ch[1]->min}); }
+void LCT::Node::push_up() {
+	min = val;
+	if (ch[0]) min = std::min(min, ch[0]->min);
+	if (ch[1]) min = std::min(min, ch[1]->min);
+}
 
 void LCT::Node::setc(LCT::Node *p, int d) {
 	ch[d] = p;
-	p->fa = this;
+	if (p) p->fa = this;
 }
 
 bool LCT::Node::d() { return fa->ch[1] == this; }
 
 bool LCT::Node::is_root() {
-	return fa == null || (fa->ch[0] != this && fa->ch[1] != this);
+	return !fa || (fa->ch[0] != this && fa->ch[1] != this);
 }
 
 void LCT::Node::update_add(Data w) {
-	if (this == null)
-		return;
 	val.first += w;
 	add += w;
 	min.first += w;
@@ -85,8 +80,8 @@ void LCT::Node::update_add(Data w) {
 
 void LCT::Node::push_down() {
 	if (add) {
-		ch[0]->update_add(add);
-		ch[1]->update_add(add);
+		if (ch[0]) ch[0]->update_add(add);
+		if (ch[1]) ch[1]->update_add(add);
 		add = 0;
 	}
 }
@@ -99,11 +94,11 @@ void LCT::Node::push_down_from_root() {
 
 void LCT::Node::rot() {
 	Node *f = fa, *ff = fa->fa;
-	int c = d(), cc = fa->d();
+	int c = d();
 	f->setc(ch[!c], c);
 	this->setc(f, !c);
-	if (ff->ch[cc] == f)
-		ff->setc(this, cc);
+	if (ff && ff->ch[c = fa->d()] == f)
+		ff->setc(this, c);
 	else
 		this->fa = ff;
 	f->push_up();
@@ -121,7 +116,7 @@ LCT::Node *LCT::Node::splay() {
 }
 
 LCT::Node *LCT::Node::access() {
-	for (Node *p = this, *q = null; p != null; q = p, p = p->fa) {
+	for (Node *p = this, *q = nullptr; p; q = p, p = p->fa) {
 		p->splay()->setc(q, 1);
 		p->push_up();
 	}

commit 1ef1c01506c7e1b55c7c0e67a7fde311bd7045da

fix bug
which is introduced in bee40b69f5163dbef8e393ef020e9e837e279c89

3rd/lct/LCT.cpp
@@ -94,11 +94,11 @@ void LCT::Node::push_down_from_root() {
 
 void LCT::Node::rot() {
 	Node *f = fa, *ff = fa->fa;
-	int c = d();
+	int c = d(), cc = (ff&&fa->d());
 	f->setc(ch[!c], c);
 	this->setc(f, !c);
-	if (ff && ff->ch[c = fa->d()] == f)
-		ff->setc(this, c);
+	if (ff && ff->ch[cc] == f)
+		ff->setc(this, cc);
 	else
 		this->fa = ff;
 	f->push_up();