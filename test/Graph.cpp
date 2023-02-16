91b1c7bb9675ebdd30402b17947e32ef2f545d57
Eastern Li <EasternLi@outlook.com>
2023-02-16 11:12:05 +0800

    optimize efficiency of Graph::Graph

--- test/Graph.cpp
+++ test/Graph.cpp
@@ -3,7 +3,7 @@
 #include <sstream>
 
 Graph::Graph(int n, int m, std::vector<Node>node, std::vector<Edge> edge)
-: n_(n), m_(m), node_(node), edge_(edge) {}
+: n_(n), m_(m), node_(std::move(node)), edge_(std::move(edge)) {}
 
 void Graph::prettyPrint(std::string &out) const {
 	if (!info.empty()) {
